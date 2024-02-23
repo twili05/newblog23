315392233063d2723c35d3b74b605ddc7f0fb641
Moumita <moumitaKanrar526@gmail.com>
2024-02-23 21:28:12 +0530

    23:created new project

---
+++ src/main/java/com/newblog23/newblog23/Newblog23Application.java
@@ -0,0 +1,13 @@
+package com.newblog23.newblog23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newblog23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newblog23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/newblog23/newblog23/Newblog23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newblog23.newblog23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Newblog23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
